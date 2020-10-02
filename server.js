2d3c9184fbfa93138bc2e5bfd65ed390a45b7b06
Jonathan Sifleet <41328055+JonathanMSifleet@users.noreply.github.com>
2020-10-02 14:45:48 +0100

    Create dumb server

--- server.js
+++ server.js
@@ -14,3 +14,19 @@ process.on('uncaughtException', (err) => {
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
+
+// connection
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+// connect to MongoDB database:
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('DB Connection successful!'));
