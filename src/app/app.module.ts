f2054714639ccca1859c21cf6c4514a2d66fbf63
Jonathan Sifleet <41328055+JonathanMSifleet@users.noreply.github.com>
2020-10-06 17:07:37 +0100

    Display header and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { PageHeaderComponent } from './page-header/page-header.component';
+import { PageFooterComponent } from './page-footer/page-footer.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
+  declarations: [AppComponent, HomeComponent, PageHeaderComponent, PageFooterComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
