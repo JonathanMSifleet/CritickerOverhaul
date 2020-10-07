333b0a55bb169ddd03bd76415baa6cd7b3f906eb
Jonathan Sifleet <41328055+JonathanMSifleet@users.noreply.github.com>
2020-10-07 11:06:55 +0100

    Allow routing to multiple pages

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,30 @@
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router'; // CLI imports router
+
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: HomeComponent
+  },
+  {
+    path: 'login',
+    pathMatch: 'full',
+    component: LoginComponent
+  },
+  {
+    path: 'signup',
+    pathMatch: 'full',
+    component: SignupComponent
+  }
+]; // sets up routes constant where you define your routes
+
+// configures NgModule imports and exports
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,20 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { PageHeaderComponent } from './page-header/page-header.component';
 import { PageFooterComponent } from './page-footer/page-footer.component';
+import { AppRoutingModule } from './app-routing.module';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, PageHeaderComponent, PageFooterComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    PageHeaderComponent,
+    PageFooterComponent,
+    LoginComponent,
+    SignupComponent
+  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
