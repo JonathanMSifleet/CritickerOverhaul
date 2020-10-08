f61758d63e2dc03c751c043b7941a655968c019e
Jonathan Sifleet <41328055+JonathanMSifleet@users.noreply.github.com>
2020-10-08 14:03:49 +0100

    Load reviews

--- controllers/reviewController.ts
+++ controllers/reviewController.ts
@@ -29,8 +29,6 @@ exports.getAllReviews = catchAsyncErrors(
       .select({ gameName: 1, tagLine: 1, slug: 1, image: 1, createdAt: 1 })
       .sort({ createdAt: -1 }); // sort broken
 
-    console.log(reviews);
-
     res.status(201).json({
       status: 'success',
       data: {
---
+++ src/app/pages/home/review.model.ts
@@ -0,0 +1,7 @@
+export interface Review {
+  createdAt: string;
+  gameName: string;
+  image: string;
+  slug: string;
+  _id: string;
+}
