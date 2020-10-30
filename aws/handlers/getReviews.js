ecf93161b078d70eb9302c7854579a30316b0bd0
Jonathan Sifleet <41328055+JonathanMSifleet@users.noreply.github.com>
2020-10-30 16:11:54 +0000

    Attempt to load reviews from AWS

---
+++ aws/handlers/getReviews.js
@@ -0,0 +1,38 @@
+import AWS from 'aws-sdk';
+import createError from 'http-errors';
+
+// import getReviewsSchema from '../lib'
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+async function getReviews(event, context) {
+
+  let reviews;
+
+  const params = {
+    TableName: process.env.REVIEW_TABLE_NAME,
+    IndexName: 'reviewTitle'
+    // KeyConditionExpression: '#id !== :id',
+    // ExpressionAttributeValues: {
+    //   ':id': '',
+    // },
+    // ExpressionAttributeNames: {
+    //   '#id': 'id'
+    // }
+  };
+
+  try {
+    const result = await dynamodb.scan(params).promise();
+
+    reviews = result.Items;
+  } catch (e) {
+    console.error(e);
+    throw new createError.InternalServerError(e);
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({reviews})
+  };
+};
+
+export const handler = getReviews;
--- aws/handlers/logSomething.js
+++
@@ -1,8 +0,0 @@
-// import AWS from 'aws-sdk';
-// const dynamodb = new AWS.DynamoDB.DocumentClient();
-
-async function logSomething(event, context) {
-  console.log('do stuff...');
-}
-
-export const handler = logSomething;
