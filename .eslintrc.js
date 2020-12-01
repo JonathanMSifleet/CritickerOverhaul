86591f4867a9bb0be96872a9f1250db82c1de5c9
Jonathan Sifleet <41328055+JonathanMSifleet@users.noreply.github.com>
2020-12-01 13:27:08 +0000

    Fix some lint issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  parserOptions: { sourceType: 'module', ecmaVersion: 2015, ecmaFeatures: { legacyDecorators: true } },
+  parserOptions: { sourceType: 'module', ecmaVersion: 2020, ecmaFeatures: { legacyDecorators: true } },
   rules: {
     'no-console': 0,
     '@typescript-eslint/no-var-requires': 0,
