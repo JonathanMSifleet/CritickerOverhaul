fe6dab5a4cc216ffa49932bae3383b87f2a2b929
Jonathan Sifleet <41328055+JonathanMSifleet@users.noreply.github.com>
2020-12-01 13:31:05 +0000

    Misc. fixes

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,18 @@
 module.exports = {
   // Specify the entry point for our app.
-  entry: './src/main.ts',
+  entry: './angular/main.ts',
   // Specify the output file containing our bundled code
   output: {
     filename: 'bundle.js'
   },
   module: {
-    // Tell webpack how to load 'json' files. 
+    // Tell webpack how to load 'json' files.
     // When webpack encounters a 'require()' statement
     // where a 'json' file is being imported, it will use
     // the json-loader
     loaders: [
       {
-        test: /\.json$/, 
+        test: /\.json$/,
         loaders: ['json']
       }
     ]
