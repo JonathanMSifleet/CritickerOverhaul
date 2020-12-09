5971cd262bc28760fc31af4d5873bc83743c4b5e
Jonathan Sifleet <41328055+JonathanMSifleet@users.noreply.github.com>
2020-12-09 12:28:27 +0000

    Attempt to optimise webpack
    https://itnext.io/how-to-optimise-your-serverless-typescript-webpack-eslint-setup-for-performance-86d052284505

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,39 @@
 module.exports = {
-  parserOptions: { sourceType: 'module', ecmaVersion: 2020, ecmaFeatures: { legacyDecorators: true } },
-  rules: {
-    'no-console': 0,
-    '@typescript-eslint/no-var-requires': 0,
-    'no-spaced-func': 0
+  "extends": [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "standard"
+  ],
+  "env": {
+    "node": true
+  },
+  "parser": "@typescript-eslint/parser",
+  "plugins": [
+    "@typescript-eslint",
+  ],
+  "settings": {
+    "import/parsers": {
+      "@typescript-eslint/parser": [
+      ".ts",
+      ".tsx"
+    ]
+    },
+    "import/resolver": {
+      "typescript": {}
+    }
+  },
+  "parserOptions": {
+    "project": "./tsconfig.json",
+    "tsconfigRootDir": "./",
+    "sourceType": "module",
+    "ecmaVersion": 2020,
+    "ecmaFeatures": { legacyDecorators: true }
+  },
+  "rules": {
+    "@typescript-eslint/no-explicit-any": "off",
+    "no-console": 0,
+    "@typescript-eslint/no-var-requires": 0,
+    "no-spaced-func": 0
+    }
   }
-};
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,53 @@
+const nodeExternals = require('webpack-node-externals');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 module.exports = {
+  externalsPresets: { node: true }, // in order to ignore built-in modules like path, fs, etc.
+  devtool: 'cheap-module-eval-source-map',
   // Specify the entry point for our app.
   entry: './angular/main.ts',
+  resolve: {
+    extensions: ['.mjs', '.json', '.ts'],
+    symlinks: false,
+    cacheWithContext: false,
+  },
   // Specify the output file containing our bundled code
   output: {
+    libraryTarget: 'commonjs',
     filename: 'bundle.js'
   },
+  target: 'node',
+  externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
   module: {
-    // Tell webpack how to load 'json' files.
-    // When webpack encounters a 'require()' statement
-    // where a 'json' file is being imported, it will use
-    // the json-loader
+    rules: [{
+      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+      test: /\.(tsx?)$/,
+      loader: 'ts-loader',
+      exclude: [
+        [
+          'node_modules',
+          '.serverless',
+          '.webpack'
+        ]
+      ],
+      options: {
+        transpileOnly: true,
+        experimentalWatchApi: true
+      },
+    }],
+    // all files with a '.json' extension will be handled by the json-loader
     loaders: [
       {
         test: /\.json$/,
         loaders: ['json']
       }
     ]
-  }
+  },
+  plugins: [
+    new ForkTsCheckerWebpackPlugin({
+      eslint: true,
+      eslintOptions: {
+        cache: true
+      }
+    })
+  ],
 }
