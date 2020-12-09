054671f38102bc254957602c1f4007ac38c9921b
Jonathan Sifleet <41328055+JonathanMSifleet@users.noreply.github.com>
2020-12-09 15:35:04 +0000

    Finish optimising webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
+const path = require('path');
+
 const nodeExternals = require('webpack-node-externals');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+
 module.exports = {
-  externalsPresets: { node: true }, // in order to ignore built-in modules like path, fs, etc.
-  devtool: 'cheap-module-eval-source-map',
+  devtool: 'eval-cheap-module-source-map',
   // Specify the entry point for our app.
   entry: './angular/main.ts',
   resolve: {
@@ -13,32 +15,31 @@ module.exports = {
   // Specify the output file containing our bundled code
   output: {
     libraryTarget: 'commonjs',
+    path: path.join(__dirname, '.webpack'),
     filename: 'bundle.js'
   },
   target: 'node',
   externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
   module: {
-    rules: [{
-      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
-      test: /\.(tsx?)$/,
-      loader: 'ts-loader',
-      exclude: [
-        [
-          'node_modules',
-          '.serverless',
-          '.webpack'
-        ]
-      ],
-      options: {
-        transpileOnly: true,
-        experimentalWatchApi: true
-      },
-    }],
-    // all files with a '.json' extension will be handled by the json-loader
-    loaders: [
+    rules: [
       {
+        // all files with a '.json' extension will be handled by the json-loader
         test: /\.json$/,
-        loaders: ['json']
+        loader: 'json5-loader'
+      },
+      {
+        // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+        test: /\.(tsx?)$/,
+        loader: 'ts-loader',
+        exclude: [
+          [/node_modules/],
+          [/.serverless/],
+          [/.webpack/]
+        ],
+        options: {
+          transpileOnly: true,
+          experimentalWatchApi: true
+        }
       }
     ]
   },
