56f2ab50ae09a44afbcea891d6bd7f2aae238b4b
Jonathan Sifleet <41328055+JonathanMSifleet@users.noreply.github.com>
2021-06-09 15:13:13 +0100

    Check to see if TeamCity build pipeline does in fact work

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const App: React.FC = () => {
   return (
     <>
-      <h2>Welcome to React App</h2>
+      <h2>Welcome to test</h2>
       <h3>Date : {new Date().toDateString()}</h3>
     </>
   );
