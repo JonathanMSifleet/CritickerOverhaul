b69268a7cfd276539aecff2b8070bb725330a667
Jonathan Sifleet <41328055+JonathanMSifleet@users.noreply.github.com>
2021-06-09 14:21:39 +0100

    Fix TypeScript issues

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const App = () => {
+const App: React.FC = () => {
   return (
     <>
       <h2>Welcome to React App</h2>
       <h3>Date : {new Date().toDateString()}</h3>
-    <>
+    </>
   );
-}
+};
 
 export default App;
--- client/src/index.ts
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-import App from './App';
-import './App.scss';
-
-ReactDom.render(<App />, document.getElementById('app'));
