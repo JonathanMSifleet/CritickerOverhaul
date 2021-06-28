6dce2030aee0debcd2c748265ec3bd5011136838
Jonathan Sifleet <41328055+JonathanMSifleet@users.noreply.github.com>
2021-06-28 15:32:20 +0100

    Re-add global state

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,22 +1,24 @@
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import './assets/MDBootstrap/scss/mdb-free.scss';
+import './styles/global.scss';
 import React from 'react';
 import ReactDom from 'react-dom';
 import App from './App';
-import './styles/global.scss';
+import Context from './hooks/store/context';
+import StateHook from './hooks/store/StateHook';
 
-// const AppWithState = () => {
-//   const store = StateHook();
-//   return (
-//     <Context.Provider value={store}>
-//       <App />
-//     </Context.Provider>
-//   );
-// };
+const AppWithState = () => {
+  const store = StateHook();
+  return (
+    <Context.Provider value={store}>
+      <App />
+    </Context.Provider>
+  );
+};
 
 ReactDom.render(
   <React.StrictMode>
-    <App />
+    <AppWithState />
   </React.StrictMode>,
   document.getElementById('app')
 );
