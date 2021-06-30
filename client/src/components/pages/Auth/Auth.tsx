e8e5fc17fcf7cf740ef18622de199ec0ff9abf5a
Jonathan Sifleet <41328055+JonathanMSifleet@users.noreply.github.com>
2021-06-30 13:31:39 +0100

    Use reusable button component

--- client/src/components/pages/Auth/Auth.tsx
+++ client/src/components/pages/Auth/Auth.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import PageView from '../../hoc/PageLayout/PageView/PageView';
+import Button from '../../shared/Button/Button';
 import classes from './Auth.module.scss';
 import Login from './Login/Login';
 import Register from './Register/Register';
@@ -15,20 +16,19 @@ const Auth: React.FC = () => {
           role="group"
           aria-label="Basic example"
         >
-          <button
+          <Button
             className={`${classes.Button} btn btn-primary bg-primary text-white`}
             disabled={displayLoginForm ? true : false}
             onClick={() => setDisplayLoginForm(true)}
-          >
-            Login
-          </button>
-          <button
+            text="Login"
+          />
+
+          <Button
             className={`${classes.Button} btn btn-primary bg-primary text-white`}
             disabled={displayLoginForm ? false : true}
             onClick={() => setDisplayLoginForm(false)}
-          >
-            Register
-          </button>
+            text="Register"
+          />
         </div>
         {displayLoginForm ? <Login /> : <Register />}
       </div>
--- client/src/components/shared/Button/Button.tsx
+++ client/src/components/shared/Button/Button.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 
 interface IProps {
   className: string;
+  disabled?: boolean;
+  onClick?(): void;
   text?: string | JSX.Element;
 }
 
-const Button: React.FC<IProps> = ({ className, text }) => (
-  <button className={className}>{text}</button>
+const Button: React.FC<IProps> = ({ className, disabled, onClick, text }) => (
+  <button className={className} disabled={disabled} onClick={onClick}>
+    {text}
+  </button>
 );
 
 export default Button;
