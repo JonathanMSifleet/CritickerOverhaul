d84bedaae68c089f07a1ef449ce1f91439e9fb87
Jonathan Sifleet <41328055+JonathanMSifleet@users.noreply.github.com>
2021-07-14 10:48:55 +0100

    Log error on invalid route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -33,7 +33,7 @@ const App: React.FC = () => {
         <Route exact path="/terms">
           <TextOnlyPage pageName={`terms`} />
         </Route>
-        <Redirect to="/" />
+        <Redirect to="/?error=404" />
       </Switch>
     </Router>
   );
--- client/src/components/pages/Home/Home.tsx
+++ client/src/components/pages/Home/Home.tsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PageView from '../../hoc/PageLayout/PageView/PageView';
 import classes from './Home.module.scss';
+import queryString from 'query-string';
 
 const Home: React.FC = (): JSX.Element => {
+  useEffect(() => {
+    const error = queryString.parse(location.search).error;
+    if (error) {
+      console.error('Invalid Route:', error);
+    }
+  }, []);
+
   return (
     <PageView>
       <div className={classes.HomeWrapper}></div>
