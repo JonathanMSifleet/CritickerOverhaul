c9076384c1a913d1d667e6656be012c2f56f8433
Jonathan Sifleet <41328055+JonathanMSifleet@users.noreply.github.com>
2021-11-01 18:19:59 +0000

    Film Countries table now correctly links to foreign keys

--- migrateData/createFilmCountryTable.js
+++ migrateData/createFilmCountryTable.js
@@ -19,70 +19,62 @@ connection.connect((err) => {
   if (err) throw err;
   console.log('Connected to database');
 
-  let sql;
+  executeSQL('DROP TABLE IF EXISTS film_countries', 'Table dropped if exists');
 
-  sql = 'DROP TABLE IF EXISTS film_countries';
-  executeSQL(sql, 'Table dropped if exists');
-
-  sql =
-    'CREATE TABLE film_countries (country_id MEDIUMINT, imdb_title_id MEDIUMINT UNSIGNED ' +
-    'PRIMARY KEY (country_id, imdb_title_id), ' +
-    'FOREIGN KEY (country_id) REFERENCES critickeroverhaul.countries(country_id) ' +
-    'ON DELETE CASCADE ON UPDATE CASCADE, ' +
+  const sql =
+    'CREATE TABLE film_countries (imdb_title_id MEDIUMINT UNSIGNED, country_id TINYINT UNSIGNED, ' +
+    'PRIMARY KEY (imdb_title_id, country_id), ' +
     'FOREIGN KEY (imdb_title_id) REFERENCES critickeroverhaul.films(imdb_title_id) ' +
+    'ON DELETE CASCADE ON UPDATE CASCADE, ' +
+    'FOREIGN KEY (country_id) REFERENCES critickeroverhaul.countries(country_id) ' +
     'ON DELETE CASCADE ON UPDATE CASCADE)';
   executeSQL(sql, 'Table created');
 
   populateTable();
 });
 
-const executeSQL = (sql, message) => {
-  connection.query(sql, function (err, result) {
-    if (err) throw err;
-    console.log(message);
-  });
+const executeSQL = async (sql, i) => {
+  try {
+    await query(sql);
+    console.log(i);
+  } catch (e) {
+    console.error(e);
+    process.exit();
+  }
 };
 
 const populateTable = () => {
   csvtojson()
     .fromFile('./datasets/Film_Countries.csv')
     .then(async (source) => {
-      const numRows = source.length;
-
-      for (let i = 0; i < numRows; i++) {
+      for (let i = 0; i < source.length; i++) {
         const imdb_title_id = source[i]['imdb_title_id'];
+        let countries = source[i]['country'];
+        countries = countries.split(', ');
 
-        const countryRow = source[i]['country'];
-        const countryName = countryRow.split(', ');
-
-        countryName.forEach(async (el) => {
-          const selectStatement =
-            'SELECT country_id FROM critickeroverhaul.countries ' +
-            `WHERE critickeroverhaul.countries.country_name = "${el}"`;
-
-          let country_id;
-
+        for await (const curCountry of countries) {
           try {
-            const rows = await query(selectStatement);
-            country_id = rows[0]['country_id'];
-          } catch (e) {}
+            const country_id = await getCountryName(curCountry);
+            const insertStatement = `INSERT IGNORE INTO film_countries VALUES ('${imdb_title_id}', '${country_id}')`;
 
-          const insertStatement = 'INSERT INTO film_countries VALUES (?, ?)';
-
-          const items = [country_id, imdb_title_id];
-
-          insertRow(i, numRows, insertStatement, items);
-        });
+            await executeSQL(insertStatement, i);
+          } catch (e) {
+            console.error(e);
+          }
+        }
       }
     });
 };
 
-const insertRow = (i, numRows, insertStatement, items) => {
-  connection.query(insertStatement, items, (err, results, fields) => {
-    if (err) {
-      console.log('Unable to insert item at row ', i++);
-      return console.log(err);
-    }
-    console.log(`Row ${i} of ${numRows}`);
-  });
+const getCountryName = async (curCountry) => {
+  const selectStatement =
+    'SELECT country_id FROM critickeroverhaul.countries ' +
+    `WHERE critickeroverhaul.countries.country_name = '${curCountry}'`;
+
+  try {
+    const rows = await query(selectStatement);
+    return rows[0]['country_id'];
+  } catch (e) {
+    console.error(e);
+  }
 };
