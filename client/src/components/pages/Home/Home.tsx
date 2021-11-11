a11704e0573cad6105370db353309cafc33fe27d
Jonathan Sifleet <41328055+JonathanMSifleet@users.noreply.github.com>
2021-11-11 13:40:55 +0000

    Replace query-string with URLSearchParams

--- client/src/components/pages/Home/Home.tsx
+++ client/src/components/pages/Home/Home.tsx
@@ -1,11 +1,13 @@
 import React, { useEffect } from 'react';
 import PageView from '../../hoc/PageLayout/PageView/PageView';
 import classes from './Home.module.scss';
-import queryString from 'query-string';
 
 const Home: React.FC = (): JSX.Element => {
   useEffect(() => {
-    const error = queryString.parse(location.search).error;
+    const error = new URLSearchParams(
+      document.location.search.substring(1)
+    ).get('error');
+
     if (error) {
       console.error('Invalid Route:', error);
     }
