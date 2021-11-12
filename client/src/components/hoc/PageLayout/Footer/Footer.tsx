3a70d15c3902f5da2cf7e85b565939f6ff2c4be3
Jonathan Sifleet <41328055+JonathanMSifleet@users.noreply.github.com>
2021-11-12 11:24:43 +0000

    Improve footer styling

--- client/src/components/hoc/PageLayout/Footer/Footer.tsx
+++ client/src/components/hoc/PageLayout/Footer/Footer.tsx
@@ -5,33 +5,45 @@ import classes from './Footer.module.scss';
 const Footer: React.FC = (): JSX.Element => {
   return (
     <footer className={`${classes.Footer} bg-primary text-center text-white`}>
-      <div className={classes.FooterLinksWrapper}>
-        <ul className="FooterLinks list-group list-group-horizontal">
-          <Link to="/privacy" className="text-white">
-            <li className="list-group-item bg-primary">Privacy Policy</li>
-          </Link>
+      <ul
+        className={`${classes.FooterLinksWrapper} list-group list-group-horizonta`}
+      >
+        <Link to="/privacy" className={`${classes.LinkComponent} text-white`}>
+          <li className={`${classes.LinkText} list-group-item bg-primary`}>
+            Privacy Policy
+          </li>
+        </Link>
 
-          <Link to="/abuse" className="text-white">
-            <li className="list-group-item bg-primary">Abuse Policy</li>
-          </Link>
+        <Link to="/abuse" className={`${classes.LinkComponent} text-white`}>
+          <li className={`${classes.LinkText} list-group-item bg-primary`}>
+            Abuse Policy
+          </li>
+        </Link>
 
-          <Link to="/contact" className="text-white">
-            <li className="list-group-item bg-primary">Contact</li>
-          </Link>
+        <Link to="/contact" className={`${classes.LinkComponent} text-white`}>
+          <li className={`${classes.LinkText} list-group-item bg-primary`}>
+            Contact
+          </li>
+        </Link>
 
-          <Link to="/about" className="text-white">
-            <li className="list-group-item bg-primary">About us</li>
-          </Link>
+        <Link to="/about" className={`${classes.LinkComponent} text-white`}>
+          <li className={`${classes.LinkText} list-group-item bg-primary`}>
+            About us
+          </li>
+        </Link>
 
-          <Link to="/resources" className="text-white">
-            <li className="list-group-item bg-primary">RSS</li>
-          </Link>
+        <Link to="/resources" className={`${classes.LinkComponent} text-white`}>
+          <li className={`${classes.LinkText} list-group-item bg-primary`}>
+            RSS
+          </li>
+        </Link>
 
-          <Link to="/terms" className="text-white">
-            <li className="list-group-item bg-primary">Terms & Conditions</li>
-          </Link>
-        </ul>
-      </div>
+        <Link to="/terms" className={`${classes.LinkComponent} text-white`}>
+          <li className={`${classes.LinkText} list-group-item bg-primary`}>
+            Terms & Conditions
+          </li>
+        </Link>
+      </ul>
 
       <div className={`${classes.Copyright} copyright text-center`}>
         <span className={classes.CopyrightText}>
