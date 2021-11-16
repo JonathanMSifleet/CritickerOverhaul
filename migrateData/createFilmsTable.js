b179e7f366b183c6d18201c96d283ef1639a8c48
Jonathan Sifleet <41328055+JonathanMSifleet@users.noreply.github.com>
2021-11-16 20:06:46 +0000

    Connect to uni database

--- migrateData/createFilmsTable.js
+++ migrateData/createFilmsTable.js
@@ -5,10 +5,11 @@ const mysql = require('mysql2');
 const csvtojson = require('csvtojson');
 
 const connectionDetails = {
-  host: 'localhost',
-  user: 'JonathanS',
-  password: 'DatasetMigration',
-  database: 'critickeroverhaul'
+  host: 'mudfoot.doc.stu.mmu.ac.uk',
+  port: 6306,
+  user: 'sifleetj',
+  password: 'Joosderg6',
+  database: 'sifleetj'
 };
 
 const connection = mysql.createConnection(connectionDetails);
@@ -41,37 +42,36 @@ const executeSQL = (sql, message) => {
 const populateTable = () => {
   csvtojson()
     .fromFile('./datasets/IMDb_movies_usable_no_inline_commas.csv')
-    .then((source) => {
+    .then(async (source) => {
       // Fetching the data from each row
       // and inserting to the table "sample"
 
       const numRows = source.length;
       const insertStatement = `INSERT INTO films VALUES (?, ?, ?, ?, ?)`;
+      let i = 0;
 
-      for (let i = 0; i < numRows; i++) {
-        let imdb_title_id = source[i]['imdb_title_id'],
-          title = source[i]['title'],
-          year = source[i]['year'],
-          duration = source[i]['duration'],
-          description = source[i]['description'];
+      for await (const film of source) {
+        i++;
 
-        // remove unnecessary id prefix:
-        imdb_title_id = imdb_title_id.substring(2);
-
+        const { imdb_title_id, title, year, duration, description } = film;
         const items = [imdb_title_id, title, year, duration, description];
 
         // Inserting data of current row into database
-        insertRow(i, numRows, insertStatement, items);
+        await insertRow(i, numRows, insertStatement, items);
       }
     });
 };
 
-const insertRow = (i, numRows, insertStatement, items) => {
+const insertRow = async (i, numRows, insertStatement, items) => {
   connection.query(insertStatement, items, (err, results, fields) => {
     if (err) {
-      console.log('Unable to insert item at row ', i++);
-      return console.log(err);
+      console.log('Unable to insert item at row ', i);
+      console.log(err);
     }
-    console.log(`Row ${i} of ${numRows}`);
+
+    // console.log(`Inserted row ${i} of ${numRows}`);
+    console.log(
+      `${((i / numRows) * 100).toString().substring(0, 4)}% complete`
+    );
   });
 };
