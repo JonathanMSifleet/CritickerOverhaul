21eb0cc106252449daa361c2ee9fc38089051134
Jonathan Sifleet <41328055+JonathanMSifleet@users.noreply.github.com>
2021-11-23 15:52:46 +0000

    Fix right header content

--- client/src/components/hoc/PageLayout/Header/Header.tsx
+++ client/src/components/hoc/PageLayout/Header/Header.tsx
@@ -44,33 +44,29 @@ const Header: React.FC = (): JSX.Element => {
 
   return (
     <nav
-      className={`${classes.Header} navbar navbar-expand-lg bg-primary navbar-dark `}
+      className={`${classes.Header} navbar navbar-expand-lg bg-primary navbar-dark`}
     >
-      <Link to="/">
-        <img src={Logo} alt="Criticker Logo" />
-      </Link>
+      <img className={classes.Logo} src={Logo} alt="Criticker Logo" />
 
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <ul className={`${classes.LeftContent} navbar-nav me-auto mb-2 mb-lg-0`}>
           <li className="nav-item">
             <Link className="nav-link" to="/">
               Home
             </Link>
           </li>
-          <li className="nav-item">
-            <div className={`${classes.SearchWrapper} input-group rounded`}>
-              <input
-                aria-describedby="search-addon"
-                aria-label="Search"
-                className={`${classes.Search} form-control rounded`}
-                placeholder="[Placeholder] search"
-                type="search"
-              />
-              <span className="input-group-text border-0" id="search-addon">
-                <i className="fas fa-search"></i>
-              </span>
-            </div>
-          </li>
+          <div className={`${classes.SearchWrapper} input-group rounded`}>
+            <input
+              aria-describedby="search-addon"
+              aria-label="Search"
+              className={`${classes.Search} form-control rounded`}
+              placeholder="[Placeholder] search"
+              type="search"
+            />
+            <span className="input-group-text border-0" id="search-addon">
+              <i className="fas fa-search"></i>
+            </span>
+          </div>
         </ul>
       </div>
 
