7310e7eb8ad27460e653e6e4b17f9046e0ce88d7
Jonathan Sifleet <41328055+JonathanMSifleet@users.noreply.github.com>
2022-01-15 19:30:59 +0000

    Use individual package.jsons

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -44,8 +44,5 @@ module.exports = {
       }
     ]
   },
-  plugins: [
-    new HtmlWebpackPlugin({ template: './src/index.html' }),
-    new MiniCssExtractPlugin()
-  ]
+  plugins: [new HtmlWebpackPlugin({ template: './src/index.html' }), new MiniCssExtractPlugin()]
 };
