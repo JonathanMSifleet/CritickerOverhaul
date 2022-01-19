43c3d891438de817c2c4557b8937453ff71a1afd
Jonathan Sifleet <41328055+JonathanMSifleet@users.noreply.github.com>
2022-01-19 17:51:14 +0000

    Add ability to disable fetching from S3

---
+++ client/src/constants/saveMoney.ts
@@ -0,0 +1,3 @@
+const saveMoney = true;
+
+export default saveMoney;
--- client/src/utils/getUserAvatar.ts
+++ client/src/utils/getUserAvatar.ts
@@ -1,8 +1,11 @@
 import ShrugSVG from '../assets/svg/Shrug.svg';
 import { GET_USER_AVATAR } from '../constants/endpoints';
+import saveMoney from '../constants/saveMoney';
 import httpRequest from './httpRequest';
 
 const getUserAvatar = async (username: string): Promise<string> => {
+  if (saveMoney) return ShrugSVG;
+
   const url = `${GET_USER_AVATAR}/${username}`;
   const response = await httpRequest(url, 'GET');
 
