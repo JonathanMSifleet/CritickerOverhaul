6f23ea88386739fe1bda1a5e9f3f02c49448dbd6
JonathanMSifleet <jonathanmsifleet@gmail.com>
2022-04-01 18:59:50 +0100

    Generate mock reviews

---
+++ generateReviews/generateReviews.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+
+const getRandomInt = (max) => Math.floor(Math.random() * max);
+
+const usernames = JSON.parse(fs.readFileSync('mockUsernames.json')).map(username => username.name);
+const reviews = JSON.parse(fs.readFileSync('mockReviews.json')).map(review => review.review);
+const imdbIDs = JSON.parse(fs.readFileSync('myRatings.json')).map(id => id.imdbID);
+
+const generatedReviews = [];
+
+usernames.forEach (username => {
+  imdbIDs.forEach((imdbID) => {
+    if (getRandomInt(3) !== 2) return;
+
+    let userReview = undefined;
+    if (getRandomInt(5) !== 4) userReview = reviews[getRandomInt(reviews.length)];
+
+    let review = {
+      imdbID,
+      rating: getRandomInt(11) * 10,
+      username
+    };
+    if(userReview) review.review = userReview;
+
+    generatedReviews.push(review);
+  });
+});
+
+fs.writeFileSync('generatedReviews.json', JSON.stringify(generatedReviews));
