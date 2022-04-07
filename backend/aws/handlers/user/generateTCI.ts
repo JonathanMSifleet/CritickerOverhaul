f743d4cb1c6533cc9b3a89fef0e64df8ae34b8a2
JonathanMSifleet <jonathanmsifleet@gmail.com>
2022-04-07 20:10:19 +0100

    Store TCI in dynamo

--- backend/aws/handlers/user/generateTCI.ts
+++ backend/aws/handlers/user/generateTCI.ts
@@ -1,5 +1,5 @@
 import { createAWSResErr } from '../../shared/functions/createAWSResErr';
-import { DynamoDBClient, GetItemCommand } from '@aws-sdk/client-dynamodb';
+import { DynamoDBClient, GetItemCommand, PutItemCommand } from '@aws-sdk/client-dynamodb';
 import { unmarshall } from '@aws-sdk/util-dynamodb';
 import cors from '@middy/http-cors';
 import IHTTP from '../../shared/interfaces/IHTTP';
@@ -22,9 +22,6 @@ const generateTCI = async (event: {
 }): Promise<IHTTP | void> => {
   const { usernameOne, usernameTwo } = event.pathParameters;
 
-  // alphabetically sort usernames
-  // const tableHash = usernameOne < usernameTwo ? `${usernameOne}-${usernameTwo}` : `${usernameTwo}-${usernameOne}`;
-
   const userRatingRequests = [];
   userRatingRequests.push(getUserRatings(usernameOne));
   userRatingRequests.push(getUserRatings(usernameTwo));
@@ -33,25 +30,18 @@ const generateTCI = async (event: {
   if (userRatings instanceof Error) return createAWSResErr(500, 'Error getting user ratings');
 
   const matchingRatings = getMatchingUserRatings(userRatings as IUserRatings[]);
+  if (matchingRatings.length < 25) return createAWSResErr(404, 'Not enough matching ratings found');
 
   const ratingsOne = filterRatings(userRatings[0], matchingRatings);
   const ratingsTwo = filterRatings(userRatings[1], matchingRatings);
 
-  const percentageDifferences = matchingRatings.map(
-    (matchingRating) =>
-      ({
-        percentageDifference: calculateAbsolutePercentageDifference(
-          // @ts-expect-error can be used as index
-          ratingsOne[matchingRatings.indexOf(matchingRating)].ratingPercentile,
-          // @ts-expect-error can be used as index
-          ratingsTwo[matchingRatings.indexOf(matchingRating)].ratingPercentile
-        )
-      }.percentageDifference)
-  );
-  const averageDifference = (percentageDifferences.reduce((a, b) => a + b) / percentageDifferences.length).toFixed(4);
-  console.log('🚀 ~ file: generateTCI.ts ~ line 52 ~ averageDifference', averageDifference);
+  const tci = calculateTCI(matchingRatings, ratingsOne, ratingsTwo);
+  const tableHash = usernameOne < usernameTwo ? `${usernameOne}-${usernameTwo}` : `${usernameTwo}-${usernameOne}`;
 
-  return { statusCode: 200, body: JSON.stringify(`TCI: ${averageDifference}`) };
+  const updatedTCI = await updateDynamoTCI(tableHash, tci);
+  if (updatedTCI instanceof Error) return createAWSResErr(500, 'Error updating TCI');
+
+  return { statusCode: 204 };
 };
 
 export const handler = middy(generateTCI).use(cors());
@@ -64,6 +54,22 @@ const calculateAbsolutePercentageDifference = (valOne: number, valTwo: number): 
   }
 
   return Math.round(Math.abs(((valOne - valTwo) / valTwo) * 100));
+};
+
+const calculateTCI = (matchingRatings: IRating[], ratingsOne: IUserRatings, ratingsTwo: IUserRatings): number => {
+  const percentageDifferences = matchingRatings.map(
+    (matchingRating) =>
+      ({
+        percentageDifference: calculateAbsolutePercentageDifference(
+          // @ts-expect-error can be used as index
+          ratingsOne[matchingRatings.indexOf(matchingRating)].ratingPercentile,
+          // @ts-expect-error can be used as index
+          ratingsTwo[matchingRatings.indexOf(matchingRating)].ratingPercentile
+        )
+      }.percentageDifference)
+  );
+
+  return Number((percentageDifferences.reduce((a, b) => a + b) / percentageDifferences.length).toFixed(4));
 };
 
 const getMatchingUserRatings = (ratings: IUserRatings[]): IRating[] =>
@@ -100,3 +106,21 @@ const filterRatings = (userRatings: IUserRatings, matchingRatings: IRating[]): I
   userRatings.ratings.filter((rating: { imdbID: number }) =>
     matchingRatings.some((matchingRating) => matchingRating.imdbID === rating.imdbID)
   );
+
+const updateDynamoTCI = async (tableHash: string, tci: number): Promise<IHTTP | void> => {
+  const query = {
+    TableName: process.env.TCI_TABLE_NAME!,
+    Item: {
+      usernames: { S: tableHash },
+      tci: { N: tci.toString() }
+    }
+  };
+
+  try {
+    await dbClient.send(new PutItemCommand(query));
+    console.log('Successfully updated TCI');
+    return;
+  } catch (error) {
+    if (error instanceof Error) return createAWSResErr(500, error.message);
+  }
+};
