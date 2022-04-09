c05e509df9ad04fb2c04945b7c73d1c77dea5885
JonathanMSifleet <jonathanmsifleet@gmail.com>
2022-04-09 14:29:31 +0100

    Get TCI when getting film ratings

--- backend/aws/handlers/film/getFilmRatings.ts
+++ backend/aws/handlers/film/getFilmRatings.ts
@@ -1,7 +1,8 @@
-import { DynamoDBClient, QueryCommand } from '@aws-sdk/client-dynamodb';
+import { BatchGetItemCommand, DynamoDBClient, QueryCommand } from '@aws-sdk/client-dynamodb';
 
 import { createAWSResErr } from '../../shared/functions/createAWSResErr';
 import { unmarshall } from '@aws-sdk/util-dynamodb';
+import chunk from 'chunk';
 import cors from '@middy/http-cors';
 import createDynamoSearchQuery from '../../shared/functions/queries/createDynamoSearchQuery';
 import IHTTP from '../../shared/interfaces/IHTTP';
@@ -10,13 +11,16 @@ import middy from '@middy/core';
 
 const dbClient = new DynamoDBClient({});
 
-const getFilmRatings = async (event: { pathParameters: { imdbID: number } }): Promise<IHTTP | undefined> => {
-  const imdbID = event.pathParameters.imdbID;
+const getFilmRatings = async (event: {
+  pathParameters: { imdbID: number; username?: string };
+}): Promise<IHTTP | undefined> => {
+  const { username, imdbID } = event.pathParameters;
 
   const ratings = await getFilmRatingsFromDB(imdbID);
   if (ratings instanceof Error) return createAWSResErr(500, 'Error retrieving ratings');
+  if ((ratings as IRating[]).length === 0) return createAWSResErr(404, 'No ratings');
 
-  if ((ratings as IRating[]).length === 0) return createAWSResErr(404, 'Error retrieving ratings');
+  if (username !== undefined) await getTCIs(username, ratings as IRating[]);
 
   return {
     statusCode: 200,
@@ -45,4 +49,34 @@ const getFilmRatingsFromDB = async (imdbID: number): Promise<IRating[] | IHTTP> 
   }
 
   return createAWSResErr(500, 'Unhandled Exception');
+};
+
+const getTCIs = async (curUsername: string, ratings: IRating[]): Promise<IHTTP | void> => {
+  const usernamePairs: string[] = [];
+
+  const usernames = ratings.map((rating) => rating.username);
+
+  usernames.forEach((username) => {
+    username < curUsername
+      ? usernamePairs.push(`${username}-${curUsername}`)
+      : usernamePairs.push(`${curUsername}-${username}`);
+  });
+
+  const usernameItems = usernames.map((usernamePair) => ({ usernames: { S: usernamePair } }));
+  const usernameItemBatch = chunk(usernameItems, 100);
+
+  for await (const batch of usernameItemBatch) {
+    const params = {
+      RequestItems: {
+        [process.env.TCI_TABLE_NAME!]: { Keys: batch }
+      }
+    };
+
+    try {
+      const result = await dbClient.send(new BatchGetItemCommand(params));
+      console.log('🚀 ~ file: getFilmRatings.ts ~ line 77 ~ forawait ~ result', result);
+    } catch (error) {
+      if (error instanceof Error) return createAWSResErr(500, error.message);
+    }
+  }
 };
