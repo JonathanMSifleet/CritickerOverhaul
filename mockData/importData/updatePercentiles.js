62e08b36de12dfeb0184b2d008f13ed7a5002bba
JonathanMSifleet <jonathanmsifleet@gmail.com>
2022-04-19 19:08:25 +0100

    Convert updatePercentiles function to regular JS function

---
+++ mockData/importData/updatePercentiles.js
@@ -0,0 +1,25 @@
+const endpoints = require('./endpoints');
+const fetch = require('node-fetch');
+const fs = require('fs');
+
+(async () => {
+  const usernames = JSON.parse(fs.readFileSync('../sharedData/mockAccounts.json'))
+    .map((username) => username.username);
+
+  console.log('Starting updating');
+
+  const importRequests = [];
+  usernames.forEach((username) =>
+    importRequests.push(fetch(`${endpoints.UPDATE_PERCENTILES}/${username}`, {
+      method: 'PUT'
+    }))
+  );
+
+  try {
+    await Promise.all(importRequests);
+  } catch (error) {
+    console.error(error);
+  }
+
+  console.log('Update completed');
+})();
