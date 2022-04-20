1bc28aedac3f1dafb1f8e711223891a6895f4f7d
JonathanMSifleet <jonathanmsifleet@gmail.com>
2022-04-20 14:40:58 +0100

    Convert aggregatePercentiles to regular JS

---
+++ mockData/importData/aggregatePercentiles.js
@@ -0,0 +1,24 @@
+const endpoints = require('./endpoints');
+const fetch = require('node-fetch');
+const fs = require('fs');
+
+(async () => {
+  const usernames = JSON.parse(fs.readFileSync('../sharedData/mockAccounts.json')).map((username) => username.username);
+
+  console.log('Starting import');
+
+  const updateRequests = [];
+  usernames.forEach((username) => updateRequests.push(
+    fetch(`${endpoints.AGGREGATE_PERCENTILES}/${username}`, {
+      method: 'PUT'
+    })
+  ));
+
+  try {
+    await Promise.all(updateRequests);
+  } catch (error) {
+    console.error(error);
+  }
+
+  console.log('Import completed');
+})();
