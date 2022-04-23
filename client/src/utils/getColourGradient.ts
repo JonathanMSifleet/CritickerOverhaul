765f9d4ca4ebd6c4fd7a0cabde367760a8448c7d
JonathanMSifleet <jonathanmsifleet@gmail.com>
2022-04-23 15:35:19 +0100

    Fix incorrect colour gradients

--- client/src/utils/getColourGradient.ts
+++ client/src/utils/getColourGradient.ts
@@ -1,7 +1,10 @@
-import colourArray from '../constants/colourArray';
+import colourGradient from 'javascript-color-gradient';
 
-const getColourGradient = (ratingPercentile: number): string => {
-  let colourIndex = Math.round(ratingPercentile / 10);
+// @ts-expect-error setGradient does exist:
+const colourArray = colourGradient.setGradient('#C70000', '#FBFB13', '#228A00');
+
+const getColourGradient = (value: number): string => {
+  let colourIndex = Math.round(value / 10);
   if (colourIndex <= 0) colourIndex = 1;
 
   return colourArray.getColor(colourIndex);
