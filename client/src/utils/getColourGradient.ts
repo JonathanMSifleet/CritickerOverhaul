14faa934c8aa3905d2511b37ad3b44dbdee606b4
JonathanMSifleet <jonathanmsifleet@gmail.com>
2022-05-01 12:53:12 +0100

    Update dependencies

--- client/src/utils/getColourGradient.ts
+++ client/src/utils/getColourGradient.ts
@@ -1,9 +1,8 @@
-import colourGradient from 'javascript-color-gradient';
-
-// @ts-expect-error setGradient does exist:
-const colourArray = colourGradient.setGradient('#C70000', '#FBFB13', '#228A00');
+import Gradient from 'javascript-color-gradient';
 
 const getColourGradient = (value: number): string => {
+  const colourArray = new Gradient().setColorGradient('#C70000', '#FBFB13', '#228A00');
+
   let colourIndex = Math.round(value / 10);
   if (colourIndex <= 0) colourIndex = 1;
 
