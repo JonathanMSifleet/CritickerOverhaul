b0039a2a11ba3f2a49040a4022a59f00efc0e8cd
JonathanMSifleet <jonathanmsifleet@gmail.com>
2022-05-03 15:43:16 +0100

    Remove unused function from get film ratings lambda

--- backend/aws/handlers/film/getFilmRatings.ts
+++ backend/aws/handlers/film/getFilmRatings.ts
@@ -1,7 +1,6 @@
-import { BatchGetItemCommand, DynamoDBClient, QueryCommand } from '@aws-sdk/client-dynamodb';
 import { createAWSResErr } from '../../shared/functions/createAWSResErr';
+import { DynamoDBClient, QueryCommand } from '@aws-sdk/client-dynamodb';
 import { unmarshall } from '@aws-sdk/util-dynamodb';
-import chunk from 'chunk';
 import cors from '@middy/http-cors';
 import createDynamoSearchQuery from '../../shared/functions/queries/createDynamoSearchQuery';
 import IHTTP from '../../shared/interfaces/IHTTP';
@@ -10,16 +9,12 @@ import middy from '@middy/core';
 
 const dbClient = new DynamoDBClient({});
 
-const getFilmRatings = async (event: {
-  pathParameters: { imdbID: number; username?: string };
-}): Promise<IHTTP | undefined> => {
-  const { username, imdbID } = event.pathParameters;
+const getFilmRatings = async (event: { pathParameters: { imdbID: number } }): Promise<IHTTP> => {
+  const { imdbID } = event.pathParameters;
 
   const ratings = await getFilmRatingsFromDB(imdbID);
   if (ratings instanceof Error) return createAWSResErr(500, 'Error retrieving ratings');
   if ((ratings as IRating[]).length === 0) return createAWSResErr(404, 'No ratings');
-
-  if (username !== undefined) await getTCIs(username, ratings as IRating[]);
 
   return {
     statusCode: 200,
@@ -48,34 +43,4 @@ const getFilmRatingsFromDB = async (imdbID: number): Promise<IRating[] | IHTTP> 
   }
 
   return createAWSResErr(500, 'Unhandled Exception');
-};
-
-const getTCIs = async (curUsername: string, ratings: IRating[]): Promise<IHTTP | void> => {
-  const usernamePairs: string[] = [];
-
-  const usernames = ratings.map((rating) => rating.username);
-
-  usernames.forEach((username) => {
-    username < curUsername
-      ? usernamePairs.push(`${username}-${curUsername}`)
-      : usernamePairs.push(`${curUsername}-${username}`);
-  });
-
-  const usernameItems = usernames.map((usernamePair) => ({ usernames: { S: usernamePair } }));
-  const usernameItemBatch = chunk(usernameItems, 100);
-
-  for await (const batch of usernameItemBatch) {
-    const params = {
-      RequestItems: {
-        [process.env.TCI_TABLE_NAME!]: { Keys: batch }
-      }
-    };
-
-    try {
-      const result = await dbClient.send(new BatchGetItemCommand(params));
-      console.log('🚀 ~ file: getFilmRatings.ts ~ line 77 ~ forawait ~ result', result);
-    } catch (error) {
-      if (error instanceof Error) return createAWSResErr(500, error.message);
-    }
-  }
 };
