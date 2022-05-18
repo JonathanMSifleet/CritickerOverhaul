384effd2fb44a54f220ace660ce69febf01037cf
JonathanMSifleet <jonathanmsifleet@gmail.com>
2022-05-18 16:56:47 +0100

    Get recent ratings MySQL

---
+++ backend/aws/shared/constants/ConnectionDetails.ts
@@ -0,0 +1,8 @@
+export const connectionDetails = {
+  host: 'mudfoot.doc.stu.mmu.ac.uk',
+  port: 6306,
+  user: 'sifleetj',
+  password: 'Joosderg6',
+  database: 'sifleetj',
+  connUtilization: 1
+};
