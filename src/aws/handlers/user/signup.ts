7f63eac9f2e15a2133e2a601f1fbce54fcc731b5
Jonathan Sifleet <41328055+JonathanMSifleet@users.noreply.github.com>
2023-08-14 09:23:38 +0100

    chore: Remove email verification

--- src/aws/handlers/user/signup.ts
+++ src/aws/handlers/user/signup.ts
@@ -2,13 +2,8 @@ import createAWSResErr from '../../shared/functions/createAWSResErr';
 import { DynamoDBClient, PutItemCommand, PutItemCommandOutput } from '@aws-sdk/client-dynamodb';
 import { marshall } from '@aws-sdk/util-dynamodb';
 import cors from '@middy/http-cors';
-import generateAccessToken from '../../shared/functions/generateAccessToken';
 import IHTTP from '../../interfaces/IHTTP';
 import middy from '@middy/core';
-import sendEmail from '../../shared/functions/sendEmail';
-import sendGrid from '@sendgrid/mail';
-import storeVerificationToken from '../../shared/functions/storeVerificationToken';
-import IAccessToken from '../../interfaces/IAccessToken';
 import { validateUserInputs } from '../../shared/functions/validationFunctions';
 import bcrypt from 'bcryptjs';
 
@@ -25,25 +20,12 @@ const signup = async (event: { body: string }): Promise<IHTTP> => {
   if (errors.length !== 0) return createAWSResErr(422, errors as string[]);
 
   const memberSince = getSignupDate();
-  const accessToken = (await generateAccessToken()) as IAccessToken;
 
   try {
     const salt = bcrypt.genSaltSync(10);
-    await insertUserToDB(username, email, bcrypt.hashSync(password, salt), memberSince, accessToken);
+    await insertUserToDB(username, email, bcrypt.hashSync(password, salt), memberSince);
 
     console.log('Signed up successfully');
-
-    const token = await storeVerificationToken(dbClient, username);
-
-    const emailContent =
-      '<p>Please go to the link below to verify your email address:</p><br>' +
-      `<p>https://dpldmuafup7cw.cloudfront.net/#/verifyEmail/${username}/${token}</p>`;
-
-    try {
-      await sendEmail(sendGrid, email, 'Criticker Overhaul - Verify email address', emailContent);
-    } catch (error) {
-      return createAWSResErr(500, 'Failed to send verification email');
-    }
 
     return {
       statusCode: 201,
@@ -64,13 +46,11 @@ const insertUserToDB = async (
   username: string,
   email: string,
   password: string,
-  memberSince: number,
-  accessToken: IAccessToken
+  memberSince: number
 ): Promise<PutItemCommandOutput> => {
   const params = {
     TableName: process.env.USER_TABLE_NAME!,
     Item: marshall({
-      accessToken: JSON.stringify(accessToken),
       email,
       isVerified: false,
       memberSince,
